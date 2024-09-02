19a3143bb8f01b62eb53e1559825cc0a5c1bccea
Zakhar <zakhar.dm.ivanov@gmail.com>
2024-09-02 18:47:05 +0300

    #1 - Произвел настройку Git (Почта и имя пользователя) для будующих коммитов

---
+++ src/main/java/com/example/CalcConvert/CalcConvertApplication.java
@@ -0,0 +1,13 @@
+package com.example.CalcConvert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcConvertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcConvertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CalcConvert/CalcConvertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CalcConvert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcConvertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
